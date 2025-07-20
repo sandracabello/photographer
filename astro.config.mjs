b108b404d908fb8dc0578b9712e0d5c935f3ea52
Sandra Cabello <xandra.cj@gmail.com>
2025-07-20 21:04:37 +0200

    hotfix: set correct url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-console.log('BASE PATH:', process.env.PUBLIC_BASE_URL);
-
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sandracabello.github.io/',
     output:'static',
-    base: process.env.PUBLIC_BASE_URL || './',
+    base: (process.env.PUBLIC_BASE_URL || '/').replace(/\/+$/, ''),
 });
