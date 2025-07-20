63f9131c9bb11aee0dc613458912d31eeca2a436
Sandra Cabello <xandra.cj@gmail.com>
2025-07-20 20:46:49 +0200

    hotfix: configuration env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+console.log('BASE PATH:', process.env.PUBLIC_BASE_URL);
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://sandracabello.github.io/',
